eccdf7b6802997378397944e20626755805642d0
bala-amypo <bala@amypo.in>
2025-12-26 06:53:37 +0000

    User commit

--- src/main/java/com/example/demo/entity/HoldingRecord.java
+++ src/main/java/com/example/demo/entity/HoldingRecord.java
@@ -1,68 +1,61 @@
 package com.example.demo.entity;
 
-import com.example.demo.entity.enums.AssetClassType;
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
+
+import com.example.demo.entity.enums.AssetClassType;
+
+import jakarta.persistence.*;
 
 @Entity
 public class HoldingRecord {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-
     private Long investorId;
-
-    @Enumerated(EnumType.STRING)
     private AssetClassType assetClass;
-
     private Double currentValue;
-
     private LocalDateTime snapshotDate = LocalDateTime.now();
-
-    public void validate() {
-        if (currentValue <= 0) {
-            throw new IllegalArgumentException("must be > 0");
-        }
+    public HoldingRecord() {
+    }
+    
+    public HoldingRecord(Long id, Long investorId, AssetClassType assetClass, Double currentValue,
+            LocalDateTime snapshotDate) {
+        this.id = id;
+        this.investorId = investorId;
+        this.assetClass = assetClass;
+        this.currentValue = currentValue;
+        this.snapshotDate = snapshotDate;
     }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Long getInvestorId() {
         return investorId;
     }
-
     public void setInvestorId(Long investorId) {
         this.investorId = investorId;
     }
-
     public AssetClassType getAssetClass() {
         return assetClass;
     }
-
     public void setAssetClass(AssetClassType assetClass) {
         this.assetClass = assetClass;
     }
-
     public Double getCurrentValue() {
         return currentValue;
     }
-
     public void setCurrentValue(Double currentValue) {
         this.currentValue = currentValue;
     }
-
     public LocalDateTime getSnapshotDate() {
         return snapshotDate;
     }
-
     public void setSnapshotDate(LocalDateTime snapshotDate) {
         this.snapshotDate = snapshotDate;
-    }
+    } 
+    
 }
